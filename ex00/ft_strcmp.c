5bdd0bd89ebef8378007de2974761054605397e0
Vinícius Lima <vinislima@gmail.com>
2024-11-19 23:27:00 -0300

    update comments

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -3,24 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vinda-si <vinda-si@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vinda-si <vinda-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 09:34:21 by vinda-si          #+#    #+#             */
-/*   Updated: 2024/09/03 17:47:59 by vinda-si         ###   ########.fr       */
+/*   Updated: 2024/11/19 19:55:29 by vinda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// Essa função têm como objetivo comparar duas strings
+// e retornar um valor inteiro que representa a diferença
+// entre elas, caso não haja diferença, a função retornará 0.
 int	ft_strcmp(char *s1, char *s2);
 
 int	ft_strcmp(char *s1, char *s2)
 {
+	// realizo a declaração de uma variável que será utilizada
+	// como um contador
 	int	count;
-
+	// atribuímos o valor de zero ao contador, para percorrer as strings
 	count = 0;
+	// enquanto os elementos das strings forem iguais e diferentes de nulo
+	// o laço será executado
 	while ((s1[count] == s2[count])
 		&& (s1[count] != '\0') && (s2[count] != '\0'))
 	{
+		// enquanto a condição for verdadeira, o contador é incrementado
 		count++;
 	}
+	// ao final do laço, é retornado a diferença entre os elementos
 	return (s1[count] - s2[count]);
 }
--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -3,43 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vinda-si <vinda-si@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vinda-si <vinda-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 13:54:13 by vinda-si          #+#    #+#             */
-/*   Updated: 2024/09/04 16:48:48 by vinda-si         ###   ########.fr       */
+/*   Updated: 2024/11/19 23:26:26 by vinda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// A função ft_strncmp compara duas strings até o limite de n caracteres.
 int	ft_strncmp(char *s1, char *s2, unsigned int n);
+// A função ft_strlen calcula o tamanho da string.
 int	ft_strlen(char *str);
-
+// Essa função recebe uma string e calcula o tamanho dela.
 int	ft_strlen(char *str)
 {
+	// declaramos um inteiro que será o contador.
 	int	i;
-
+	// atribuímos o valor 1 para o contador
 	i = 1;
+	// enquanto o caractere for diferente de '\0'
+	// o laço continuará.
 	while (str[i] != '\0')
 	{
+		// o contador é incrementado.
 		i++;
 	}
+	// ao final, o contador retorna o tamanho da string.
 	return (i);
 }
-
+// A função ft_strncmp compara duas strings até o limite de n caracteres.
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
+	// declaramos três variáveis.
+	// um unsigned int para contar o número de caracteres.
 	unsigned int	count;
+	// dois int para contar o tamanho das strings.
 	int				count_s1;
 	int				count_s2;
-
+	// atribuímos o tamanho das strings para as variáveis
+	// utilizando a função ft_strlen
 	count_s1 = ft_strlen(s1);
 	count_s2 = ft_strlen(s2);
+	// se n for menor ou igual a 0, a função retorna 0
 	if (n <= 0)
 		return (0);
+	// iniciamos o contador com 0
 	count = 0;
+	// enquanto a string 1 for igual a string 2 e o contador for menor que n - 1
+	// e as strings não forem nulas o laço continuará.
 	while ((s1[count] == s2[count]) && (count < n - 1)
 		&& (s1[count] && s2[count]))
 	{
+			// o contador é incrementado
 			count++;
 	}
+	// quando o laço terminar, a função retornará a diferença entre os caracteres
 	return (s1[count] - s2[count]);
 }
