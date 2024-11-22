0b379034ac07675267792b97701b4a85371ac4f2
Vinícius Lima <vinislima@gmail.com>
2024-11-22 20:38:29 -0300

    update ex00

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: vinda-si <vinda-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 09:34:21 by vinda-si          #+#    #+#             */
-/*   Updated: 2024/11/19 19:55:29 by vinda-si         ###   ########.fr       */
+/*   Updated: 2024/11/22 20:37:55 by vinda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,3 +33,44 @@ int	ft_strcmp(char *s1, char *s2)
 	// ao final do laço, é retornado a diferença entre os elementos
 	return (s1[count] - s2[count]);
 }
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int	main(void)
+// {
+// 	// Test cases
+// 	char *test_cases[][2] = {
+// 		{"hello", "hello"},
+// 		{"hello", "world"},
+// 		{"apple", "apples"},
+// 		{"", ""},
+// 		{"", "non-empty"},
+// 		{"non-empty", ""},
+// 		{"abc", "abz"},
+// 		{"same", "same"},
+// 		{NULL, NULL} // Marcador para o fim dos testes
+// 	};
+
+// 	int i = 0; // contador para percorrer os testes
+// 	while (test_cases[i][0] != NULL && test_cases[i][1] != NULL)
+// 	{
+// 		char *s1 = test_cases[i][0];
+// 		char *s2 = test_cases[i][1];
+		
+// 		// Resultados das funções
+// 		int result_ft = ft_strcmp(s1, s2);
+// 		int result_lib = strcmp(s1, s2);
+
+// 		// Exibição dos resultados
+// 		printf("Test %d:\n", i + 1);
+// 		printf("  Strings: \"%s\" vs \"%s\"\n", s1, s2);
+// 		printf("  ft_strcmp: %d | strcmp: %d\n", result_ft, result_lib);
+// 		printf("  Match: %s\n", (result_ft == result_lib) ? "Yes" : "No");
+// 		printf("\n");
+
+// 		i++;
+// 	}
+
+// 	return (0);
+// }
